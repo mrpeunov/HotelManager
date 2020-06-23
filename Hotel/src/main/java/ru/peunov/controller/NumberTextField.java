425c4d9b16177ba09d6add1fdaa57ae61b4aad34
Vitaly <mr.peunov@gmail.com>
2020-06-23 22:35:12 +0300

    Allmost done create new worker

---
+++ Hotel/src/main/java/ru/peunov/controller/NumberTextField.java
@@ -0,0 +1,33 @@
+package ru.peunov;
+
+import javafx.scene.control.TextField;
+
+import java.util.regex.Pattern;
+
+public class NumberTextField extends TextField {
+
+    private static Pattern decimalPattern = Pattern.compile("[-]?[0-9]*(\\,[0-9]*)?");
+
+    @Override
+    public void replaceText(int start, int end, String text) {
+        if (validate(start, text)) {
+            super.replaceText(start, end, text);
+        }
+    }
+
+    @Override
+    public void replaceSelection(String text) {
+        if (validate(Integer.MAX_VALUE, text)) {
+            super.replaceSelection(text);
+        }
+    }
+
+    private boolean validate(int start, String text) {
+        String currentText = (getText() == null) ? "" : getText();
+        if (start == 0) { //to handle "-1.1" or ".1" cases
+            return decimalPattern.matcher(text + currentText).matches();
+        } else {
+            return decimalPattern.matcher(currentText + text).matches();
+        }
+    }
+}
